0cc0610574204d0905e9bda08a9101abb903e9f9
aguspecuch <agustina.pecuch@hotmail.com>
2021-07-01 14:37:30 -0300

    Creo las 3 entities (Categoria, Pregunta y Respuesta) con sus respectivas anotaciones de hibernate.

---
+++ src/main/java/ar/com/ada/api/questionados/entities/Categoria.java
@@ -0,0 +1,51 @@
+package ar.com.ada.api.questionados.entities;
+
+import java.util.*;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name="categoria")
+public class Categoria {
+    
+    @Id
+    @Column(name="categoria_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer categoriaId;
+
+    private String nombre;
+    
+    private String descripcion;
+
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonIgnore
+    private List<Pregunta> preguntas = new ArrayList<>();
+
+    public List<Pregunta> getPreguntas() {
+        return preguntas;
+    }
+    public void setPreguntas(List<Pregunta> preguntas) {
+        this.preguntas = preguntas;
+    }
+    public Integer getCategoriaId() {
+        return categoriaId;
+    }
+    public void setCategoriaId(Integer categoriaId) {
+        this.categoriaId = categoriaId;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+   
+}
---
+++ src/main/java/ar/com/ada/api/questionados/entities/Respuesta.java
@@ -0,0 +1,52 @@
+package ar.com.ada.api.questionados.entities;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name="respuesta")
+public class Respuesta {
+    
+    @Id
+    @Column(name="respuesta_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer respuestaId;
+
+    private String texto;
+
+    @Column(name="es_correcta")
+    private boolean esCorrecta;
+
+    @ManyToOne
+    @JoinColumn(name="pregunta_id", referencedColumnName = "pregunta_id")
+    @JsonIgnore
+    private Pregunta pregunta;
+    
+    public Integer getRespuestaId() {
+        return respuestaId;
+    }
+    public void setRespuestaId(Integer respuestaId) {
+        this.respuestaId = respuestaId;
+    }
+    public String getTexto() {
+        return texto;
+    }
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+    public boolean isEsCorrecta() {
+        return esCorrecta;
+    }
+    public void setEsCorrecta(boolean esCorrecta) {
+        this.esCorrecta = esCorrecta;
+    }
+    public Pregunta getPregunta() {
+        return pregunta;
+    }
+    public void setPregunta(Pregunta pregunta) {
+        this.pregunta = pregunta;
+    }
+
+    
+}
